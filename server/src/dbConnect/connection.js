5d328948b6e0e4a7d58455c667f5f7a3df38b672
jungerion <jungerion@gmail.com>
2023-06-12 23:55:51 +0545

    project started

--- server/src/dbConnect/connection.js
+++ server/src/dbConnect/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connectDb = async() => {
-    const connect = await mongoose.connect('mongodb://127.0.0.1:27017/ecommerceWebsite');
+    const connect = await mongoose.connect('mongodb://127.0.0.1:27017/aakritecommerce');
     if(connect){
       console.log("connected to mongodb")
     }else{
